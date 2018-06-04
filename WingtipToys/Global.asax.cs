45f5789efd4beaef623ca090a887ae7fa00e2e0d
dave-tyler <davet3929@gmail.com>
2018-06-04 11:58:03 -0700

    Initial commit to GitHub

---
+++ WingtipToys/Global.asax.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+using System.Web.Routing;
+using System.Web.Security;
+using System.Web.SessionState;
+using System.Data.Entity;
+using WingtipToys.Models;
+using WingtipToys.Logic;
+
+namespace WingtipToys
+{
+    public class Global : HttpApplication
+    {
+        void Application_Start(object sender, EventArgs e)
+        {
+            // Start with defaults
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            // Initialize the product database
+            Database.SetInitializer(new ProductDatabaseInitializer());
+
+            // Create custom role and user
+            RoleActions roleActions = new RoleActions();
+            roleActions.AddUserAndRole();
+
+            // Add custom routes
+            RouteActions routeActions = new RouteActions();
+            routeActions.RegisterCustomRoutes(RouteTable.Routes);
+        }
+
+        void Application_Error(object sender, EventArgs e)
+        {
+            // Code that runs when an unhandled error occurs.
+
+            // Get last error from the server
+            Exception exc = Server.GetLastError();
+
+            if (exc is HttpUnhandledException)
+            {
+                if (exc.InnerException != null)
+                {
+                    exc = new Exception(exc.InnerException.Message);
+                    Server.Transfer("ErrorPage.aspx?handler=Application_Error%20-%20Global.asax",
+                        true);
+                }
+            }
+        }
+    }
+}
